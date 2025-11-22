4fa2019adee3e35530c1dd31b26677902413ea3c
Kevin Garza <kevin_4569@msn.com>
2025-11-22 14:01:20 -0600

    Add 'use client' directive to perfil page
    Added the 'use client' directive to ensure the perfil page is treated as a client component. This change is necessary for proper usage of hooks like useAuth in Next.js.

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,10 +1,11 @@
+"use client";
+
 import { useAuth } from "@/contexts/AuthContext";
 import { UserProfileComponente } from "../../components/UserProfile";
 
 export default function Page() {
   const { user } = useAuth();
 
-  // Evita romper el componente al no tener UID
   if (!user) return <div>Cargando...</div>;
 
   return <UserProfileComponente userId={user.id} />;
